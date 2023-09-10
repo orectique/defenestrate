b366ab61793a65087fc97455e8ff2fda97e81a25
Sreehari Pulickamadhom Sreedhar <orectique@gmail.com>
2023-09-10 22:42:48 +1000

    Create def.cmd

---
+++ def.cmd
@@ -0,0 +1,17 @@
+:: Install apps
+
+winget install Google.Chrome
+winget install Microsoft.VisualStudioCode
+winget install Brave.Brave
+winget install Microsoft.PowerToys
+winget install Julialang.Julia
+:: winget install WhatsApp.WhatsApp
+winget install Microsoft.VCRedist.2022.arm64
+winget install CondaForge.Mambaforge
+winget install Posit.RStudio
+
+:: Fuck OneDrive
+
+winget uninstall Microsoft.OneDrive
+
+
