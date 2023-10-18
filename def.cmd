ba5f766afa99fcb1bf8d2c545b28286570476742
Sreehari Pulickamadhom Sreedhar <orectique@gmail.com>
2023-10-18 18:54:28 +1100

    update themeing

--- def.cmd
+++ def.cmd
@@ -1,15 +1,3 @@
-:: Install apps
-
-winget install Google.Chrome
-winget install Microsoft.VisualStudioCode
-winget install Brave.Brave
-winget install Microsoft.PowerToys
-winget install Julialang.Julia
-:: winget install WhatsApp.WhatsApp
-winget install Microsoft.VCRedist.2022.arm64
-winget install CondaForge.Mambaforge
-winget install Posit.RStudio
-
 :: Fuck OneDrive, all my homies use a spare SSD for backup
 
 winget uninstall Microsoft.OneDrive
@@ -24,6 +12,27 @@ reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explore
 
 reg.exe add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "My Music" /t REG_EXPAND_SZ /d "%USERPROFILE%\Music" /f
 
-rmdir "%USERPROFILE%\OneDrive"
+rmdir /s /q "%USERPROFILE%\OneDrive"
 
 :: Personalize 
+
+%cd%\classic.deskthemepack
+
+:: Install apps
+
+winget install Google.Chrome
+winget install Microsoft.VisualStudioCode
+winget install Brave.Brave
+winget install Microsoft.PowerToys
+winget install Julialang.Julia
+:: winget install WhatsApp.WhatsApp
+winget install Microsoft.VCRedist.2022.arm64
+winget install CondaForge.Mambaforge
+winget install Posit.RStudio
+
+:: Install R - TODO: update links to use stable links for latest release
+curl.exe --output "%USERPROFILE%\Downloads\rbase.exe" --url https://cran.r-project.org/bin/windows/base/R-4.3.1-win.exe
+curl.exe --output "%USERPROFILE%\Downloads\rtools.exe" --url https://cran.r-project.org/bin/windows/Rtools/rtools43/files/rtools43-5863-5818.exe
+
+%USERPROFILE%\Downloads\rbase.exe /SILENT /DIR="C:\Program Files\R\R-4.3.1"
+%USERPROFILE%\Downloads\rtools.exe /SILENT /DIR="C:\Rtools43"
